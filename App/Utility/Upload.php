c7d4618cdb59305be9bf3d95a012923edd3eb6c4
louis76800 <77392391+louis76800@users.noreply.github.com>
2022-05-19 22:28:16 +0200

    0013

--- App/Utility/Upload.php
+++ App/Utility/Upload.php
@@ -17,25 +17,31 @@ class Upload {
         $fileTmpName = $file['tmp_name'];
 
         $fileExtension = pathinfo($file['name'], PATHINFO_EXTENSION);
-        $pictureName = basename($fileName . '.'. $fileExtension);
+        $pictureName = basename($fileName . '.' . $fileExtension);
 
 
         $uploadPath = $currentDirectory . $uploadDirectory . $pictureName;
 
-        if (!in_array($fileExtension, $fileExtensionsAllowed)) {
-            throw new \Exception("This file extension is not allowed. Please upload a JPEG or PNG file");
-        }
+        if ($fileExtension == "") {
 
-        if ($fileSize > 4000000) {
-            throw new \Exception("File exceeds maximum size (4MB)");
-        }
 
-        $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
-
-        if ($didUpload) {
-            return $pictureName;
         } else {
-            throw new \Exception("An error occurred. Please contact the administrator.");
+            if (!in_array($fileExtension, $fileExtensionsAllowed)) {
+
+                throw new \Exception("This file extension is not allowed. Please upload a JPEG or PNG file");
+            }
+
+            if ($fileSize > 4000000) {
+                throw new \Exception("File exceeds maximum size (4MB)");
+            }
+
+            $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+            if ($didUpload) {
+                return $pictureName;
+            } else {
+                throw new \Exception("An error occurred. Please contact the administrator.");
+            }
         }
     }
 }
